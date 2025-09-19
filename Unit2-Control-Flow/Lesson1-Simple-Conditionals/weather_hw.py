6d6cbe90b6d7479374884e130149ce668e8524a8
emrgem <emrgem@bergen.org>
2025-09-19 11:55:43 -0400

    weather advisory starter added

---
+++ Unit2-Control-Flow/Lesson1-Simple-Conditionals/weather_hw.py
@@ -0,0 +1,25 @@
+# Unit 2 Lesson 1 Homework: Weather Advisory System
+print("Weather Advisory System")
+print("=" * 23)
+
+# Step 1: Get weather information from user
+# TODO: Get temperature as integer
+
+
+# TODO: Get sunny status and convert to boolean
+
+
+# Step 2: Display weather report
+print(f"\nWeather Report:")
+# TODO: Show temperature and sunny status
+print("-" * 18)
+
+# Step 3: Temperature advice
+# TODO: Use if-else to give clothing advice based on temperature
+
+
+# Step 4: Activity suggestions  
+# TODO: Use if-else to suggest activities based on sunny status
+
+
+print("\nStay safe and have fun!")
