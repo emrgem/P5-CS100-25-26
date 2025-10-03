524c9936f2e5a3afc4d39749595f161ca562cde1
emrgem <emrgem@bergen.org>
2025-10-03 11:56:46 -0400

    for loops continues

---
+++ Unit2-Control-Flow/Lesson5-Conditionals-Practice/practice.py
@@ -0,0 +1,8 @@
+day = input("Enter day of week: ")
+if not (day == "Monday" or day == "Tuesday" or day == "Wednesday"
+or day == "Thursday" or day == "Friday"):
+    print("Sleep in!")
+elif day == "Saturday" or day == "Sunday"
+    print("Time for school!")
+else:
+    print("Invalid Day!")
