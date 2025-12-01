3fe51f17b046ea11e66876969ca1f49fbbcc9a28
emrgem <emrgem@bergen.org>
2025-12-01 10:43:16 -0500

    entry point

---
+++ Unit3-Functions/Lesson6-Entry-Point/game.py
@@ -0,0 +1,3 @@
+from math_utils import square
+# print(f"__name__ is: {__name__}")
+print(f"Damage:{square(10)}")
---
+++ Unit3-Functions/Lesson6-Entry-Point/math_utils.py
@@ -0,0 +1,8 @@
+def square(n):
+    return n * n
+
+print("Testing1:", square(2))
+print(f"__name__ is: {__name__}")
+if __name__ == "__main__":#if run directly
+    # print(f"__name__ is: {__name__}")
+    print("Testing2:", square(5))
